9b5756e078f6d2583b4b40490120eb4953039f9d
Inamura Tamaki <tinamura0159@gmail.com>
2022-11-25 18:59:30 +0900

    ユーザー情報のuserCartsにデータを追加

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import { ChangeEvent, useState } from 'react';
+import { Item } from 'types/item';
+import { UserCart } from 'types/user';
 
 export async function getStaticPaths() {
   const req = await fetch('http://localhost:3000/api/items');
@@ -18,7 +20,11 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps({ params }: { params: any }) {
+export async function getStaticProps({
+  params,
+}: {
+  params: { id: string };
+}) {
   const id = params.id;
   const req = await fetch(`http://localhost:3000/api/items/${id}`);
   const data = await req.json();
@@ -30,153 +36,151 @@ export async function getStaticProps({ params }: { params: any }) {
   };
 }
 
-export default function Item({ item }: { item: any }) {
-  const [state, setState] = useState<any>({
-    cart: { items: [], total: 0 },
-  });
-
-  const [val, setVal] = useState(item.twoDaysPrice);
-  const [period, setPeriod] = useState('');
+export default function ItemDetail({ item }: { item: Item }) {
+  const [price, setPrice] = useState(0);
+  // const [userCart, setUserCart] = useState<UserCart[]>([]);
+  const [period, setPeriod] = useState(0);
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setVal(e.target.value);
+    let num = Number(e.target.value);
+    setPeriod(num);
+    if (period === 2) {
+      setPrice(item.twoDaysPrice);
+    } else {
+      setPrice(item.sevenDaysPrice);
+    }
   };
 
-  const handleSubmit = () => {
-    const body = {
+  // const addItem = async () => {
+  // console.log('hoge');
+  // };
+
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    const req = await fetch(`http://localhost:3000/api/users/2`);
+    const data = await req.json();
+    const res = data.userCarts;
+    console.log(res);
+
+    let userCarts: UserCart = {
       id: item.id,
-      itemName: item.fesName + item.airtist,
-      retalPriod: period,
-      price: val,
+      itemName: item.artist,
+      rentalPeriod: period,
+      price: price,
+      itemImage: item.itemImage,
     };
 
-    fetch(`http://localhost:3000/api/users/}`),
-      {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(body),
-      };
-  };
+    res.push(userCarts);
+    // setUserCart(res);
+    const body = { userCarts: res };
+    // console.log(userCart)
 
-  const body = {
-    id: item.id,
-    itemName: item.fesName + item.artists,
-  };
-
-  fetch(`http://localhost:3000/api/users`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(body),
-  })
-    .then((res) => res.json())
-    .then((result) => {
-      console.log('Success', result);
-    })
-    .catch((error) => {
-      console.log('Error', error);
-    });
-  // カートへの商品の追加
-  const addItem = (item: any) => {
-    let { items } = state.cart;
-    setState({
-      cart: {
-        items: [...items, item],
-        total: state.cart.total + item.price,
+    fetch(`http://localhost:3000/api/users/2`, {
+      // user情報はgetserverside
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
       },
-    });
-   };
+      body: JSON.stringify(body),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log('Success', result);
+      })
+      .catch((error) => {
+        console.log('Error', error);
+      });
+  };
 
   return (
-    <div>
-      <div className="detail-container">
-        <div className="detail-img-wrapper">
-          <Image
-            className="detail-img"
-            src={item.itemImage}
-            alt="アー写"
-            layout="fill"
-            objectFit="cover"
-          />
-        </div>
-        <form action="patch" method="patch" onSubmit={handleSubmit}>
-          <p className="detail-title">{item.airtist}</p>
-          <div className="detail-body">
-            <div className="detail-body-inner">
-              <p>{item.itemDetail}</p>
-              <p>{item.fesName}</p>
-              <p>{item.playTime}分</p>
+    <>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div className="detail-container">
+            <div className="detail-img-wrapper">
+              <Image
+                className="detail-img"
+                src={item.itemImage}
+                alt="画像"
+                layout="fill"
+                objectFit="cover"
+              />
             </div>
-            <div className="detail-btn-wrapper">
-              <label htmlFor="palyTime">
-                <input
-                  type="radio"
-                  name="palyTime"
-                  value={item.twoDaysPrice}
-                  onChange={(e) => handleChange(e)}
-                />
-                48時間&nbsp;¥{item.twoDaysPrice}円
-              </label>
-              <br />
-              <label htmlFor="palyTime">
-                <input
-                  type="radio"
-                  name="palyTime"
-                  value={item.sevenDaysPrice}
-                  onChange={(e) => handleChange(e)}
-                />
-                7泊&nbsp;¥{item.sevenDaysPrice}円
-              </label>
-              <br />
-              <button
-                type="submit"
-                className="detail-btn"
-                onClick={() => addItem(item)}
-              >
-                カートに追加
-              </button>
+            <p className="detail-title">{item.artist}</p>
+            <div className="detail-body">
+              <div className="detail-body-inner">
+                <p>{item.itemDetail}</p>
+                <p>{item.fesName}</p>
+                <p>{item.palyTime}分</p>
+              </div>
+              <div className="detail-btn-wrapper">
+                <label htmlFor="palyTime">
+                  <input
+                    type="radio"
+                    name="palyTime"
+                    value={1}
+                    onChange={(e) => handleChange(e)}
+                  />
+                  48時間&nbsp;¥{item.twoDaysPrice}円
+                </label>
+                <br />
+                <label htmlFor="palyTime">
+                  <input
+                    type="radio"
+                    name="palyTime"
+                    value={7}
+                    onChange={(e) => handleChange(e)}
+                  />
+                  7泊&nbsp;¥{item.sevenDaysPrice}円
+                </label>
+                <br />
+                <button
+                  type="submit"
+                  className="detail-btn"
+                  // onClick={(e) => addItem()}
+                >
+                  カートに追加
+                </button>
+              </div>
             </div>
           </div>
-        </form>
-      </div>
-      <style jsx>{`
-        .detail-img-wrapper {
-          max-width: 100%;
-          height: 350px;
-          position: relative;
-        }
-        .detail-title {
-          font-size: 80px;
-          font-weight: bold;
-          position: absolute;
-          top: 25%;
-          left: 5%;
-          color: white;
-        }
-        .detail-container {
-          max-width: 1330px;
-        }
-        .detail-body {
-          padding: 15px;
-        }
-        .detail-img-wrapper::after {
-          content: '';
-          width: 100%;
-          height: 100%;
-          display: block;
-          position: absolute;
-          top: 0;
-          left: 0;
-          background-color: rgba(0, 0, 0, 0.4);
-        }
-        .detail-btn {
-          padding: 5px 10px;
-          border: 1px solid #333;
-          border-radius: 10px;
-        }
-      `}</style>
-    </div>
+          <style jsx>{`
+            .detail-img-wrapper {
+              max-width: 100%;
+              height: 350px;
+              position: relative;
+            }
+            .detail-title {
+              font-size: 80px;
+              font-weight: bold;
+              position: absolute;
+              top: 25%;
+              left: 5%;
+              color: white;
+            }
+            .detail-container {
+              max-width: 1330px;
+            }
+            .detail-body {
+              padding: 15px;
+            }
+            .detail-img-wrapper::after {
+              content: '';
+              width: 100%;
+              height: 100%;
+              display: block;
+              position: absolute;
+              top: 0;
+              left: 0;
+              background-color: rgba(0, 0, 0, 0.4);
+            }
+            .detail-btn {
+              padding: 5px 10px;
+              border: 1px solid #333;
+              border-radius: 10px;
+            }
+          `}</style>
+        </div>
+      </form>
+    </>
   );
 }
