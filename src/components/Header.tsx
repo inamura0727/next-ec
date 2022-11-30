4d1d6ae20f2e2c2f1703e74fc5fac1fd83d318ad
Riku Fudo <riku.fudo@rakus-partners.co.jp>
2022-11-30 15:01:49 +0900

    ヘッダーの修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,46 +8,58 @@ type headerProps = {
   dologout: () => void;
 };
 
-export default function Header({ isLoggedIn, dologout }: headerProps) {
+export default function Header({
+  isLoggedIn,
+  dologout,
+}: headerProps) {
   return (
     <header className={styles.header}>
       <div className={styles.info}>
         <Image
-          src={'/'}
-          width={100}
-          height={70}
+          src={'/images/logo.png'}
+          width={199}
+          height={60}
           alt={'タイトルロゴ'}
         />
 
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/">トップページ</Link>
+            </li>
+            <li>
+              <Link href="/search">検索</Link>
+            </li>
+            <li>
+              <Link href="/cart">カート</Link>
+            </li>
+            {isLoggedIn && (
+              <li>
+                <Link href="/mypage">マイページ</Link>
+              </li>
+            )}
+          </ul>
+        </nav>
         <span className={styles.loginInfo}>
           {isLoggedIn ? (
-            <Link
-              href="/api/logout"
-              onClick={async (e) => {
-                e.preventDefault();
-                await fetch('/api/logout').then(() => dologout());
-              }}
-            >
-              ログアウト
-            </Link>
+            <div className={styles.btnWrapper}>
+              <Link
+                href="/api/logout"
+                onClick={async (e) => {
+                  e.preventDefault();
+                  await fetch('/api/logout').then(() => dologout());
+                }}
+              >
+                ログアウト
+              </Link>
+            </div>
           ) : (
-            <Link href="/login">ログイン</Link>
+            <div className={styles.btnWrapper}>
+              <Link href="/login">ログイン</Link>
+            </div>
           )}
         </span>
       </div>
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <Link href="/top">トップページ</Link>
-          </li>
-          <li>
-            <Link href="/search">検索</Link>
-          </li>
-          <li>
-            <Link href="/cart">カート</Link>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 }
