e637711fddd1d693f25be0359a638870b72aaf9a
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2022-11-30 10:26:38 +0900

    メインをマージしました。

--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -1,16 +1,29 @@
-import Head from "next/head";
-import {Item} from "types/item"
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Item } from 'types/item';
+import styles from 'styles/top.module.css';
+import Header from '../../components/Header';
 import ItemList from "components/ItemList";
+import UseSWR, { mutate } from 'swr';
+import { SessionUser } from '../pages/api/getUser';
 
-export default function Top ({items}: {items: Array<Item>}) {
-    return(
-        <>
-        <Head>
-            <title>トップページ - Festal</title>
-        </Head>
-        <ItemList items={items}/>
-        </>
-    )
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+export default function Top({ items }: { items: Array<Item> }) {
+
+  const { data } = UseSWR<SessionUser>('/api/getUser',fetcher);
+  if(!data) return <div>Loading</div>
+
+  return (
+    <>
+      <Head>
+        <title>トップページ - Festal</title>
+      </Head>
+      <Header isLoggedIn={data?.isLoggedIn} dologout={() => mutate('/api/getUser')}/>
+      <ItemList items={items}/>
+    </>
+  );
 }
 
 export async function getServerSideProps() {
