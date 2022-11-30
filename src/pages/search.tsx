b927c5dfae5a268d8d2365fd294a4f8f744fceb4
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2022-11-30 13:38:05 +0900

    public/images/logo.pngを追加しました。 componentsの位置をsrc下に変更しました。

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { Item } from "types/item";
-import Header from "../../components/Header";
+import Header from "../components/Header";
 import SearchForm from "components/SearchForm";
 import ItemList from "components/ItemList";
 import UseSWR, { mutate } from 'swr';
