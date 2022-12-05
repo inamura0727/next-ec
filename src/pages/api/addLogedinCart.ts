c8df212b5fb70a016dcefdcdd009d8af76ef8a50
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2022-12-05 14:31:29 +0900

    改行を追加しました。

--- src/pages/api/addLogedinCart.ts
+++ src/pages/api/addLogedinCart.ts
@@ -6,29 +6,30 @@ import { ironOptions } from '../../../lib/ironOprion';
 export default withIronSessionApiRoute(addLogedinCart, ironOptions);
 
 async function addLogedinCart(req: NextApiRequest, res: NextApiResponse) {
-    if(req.session.user){
+    if (req.session.user) {
         const response = await fetch(`http://localhost:3000/api/users/${req.session.user.id}`);
         const userData: User = await response.json();
         const userCart: UserCart[] = userData.userCarts;
-        if(req.session.cart){
+        if (req.session.cart) {
             // userのカートにsessionのカートをマージ
-            
             let sessionCart = req.session.cart;
-            for(let item of sessionCart){
-                userCart.push({id: (userCart.length + 1) , itemId: item.itemId, itemName: item.itemName, rentalPeriod: item.rentalPeriod, price: item.price, itemImage: item.itemImage});
+            for (let item of sessionCart) {
+                userCart.push(
+                    { id: (userCart.length + 1), itemId: item.itemId, itemName: item.itemName, rentalPeriod: item.rentalPeriod, price: item.price, itemImage: item.itemImage }
+                );
             }
-            
+
             // sessionのカートを空にする
             sessionCart = []
         }
-        const data = { userCarts: userCart};
+        const data = { userCarts: userCart };
         await fetch(
-        `http://localhost:3000/api/users/${req.session.user.id}`,
-        {
-            method: 'PATCH',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data),
-        }
+            `http://localhost:3000/api/users/${req.session.user.id}`,
+            {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data),
+            }
         ).then(() => res.redirect('/'))
-        }
-  }
+    }
+}
