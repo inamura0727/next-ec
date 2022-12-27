20e59e9f378ba84241ceb8d8a3eba2cfacf63284
konishi <tatsuki.konishi@rakus-partners.co.jp>
2022-12-27 14:46:10 +0900

    プルリク修正

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -192,10 +192,6 @@ export default function Mypage({
                             />
                             <div className={styles.rentalInfo}>
                               <p>{`決済日：${rentalHistory.payDate.Year}年${rentalHistory.payDate.Month}月${rentalHistory.payDate.Date}日`}</p>
-                              {/* {rentalHistory.endDay &&
-                              rentalHistory.startDay &&
-                              rentalHistory.endDay !== 'null' &&
-                              rentalHistory.startDay !== 'null' ? ( */}
                               {rentalHistory.endDay &&
                               rentalHistory.startDay ? (
                                 <Countdown
